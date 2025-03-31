6357dc38e7e8baa975ba6add9a978829d6143aa0
Priyavrat <priyevratsharma@gmail.com>
2025-03-31 20:46:53 +0530

    Added layoutLoading

---
+++ src/components/LayoutLoading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'remixicon/fonts/remixicon.css';
+
+const LayoutLoading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <i className="ri-loader-5-line text-5xl animate-spin"></i>
+    </div>
+  );
+};
+
+export default LayoutLoading;
